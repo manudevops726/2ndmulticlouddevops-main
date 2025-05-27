2ccafe2d333576c6bbf8ac74cfeb0faa6c7e87cf
manudevops726 <amanasa726@gmail.com>
2025-05-27 16:32:15 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,8 +1,7 @@
- const API_BASE_URL = "http://54.166.10.229:86";
+ const API_BASE_URL = "http://54.166.10.229:90";
 //const API_BASE_URL = "http://veera.905418452515.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
 export default API_BASE_URL;
-
