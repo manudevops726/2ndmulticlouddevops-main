1d216847a573f050ab410b36c4f920fb8625eea1
manudevops726 <amanasa726@gmail.com>
2025-06-01 13:22:09 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
- const API_BASE_URL = "http://44.211.156.103:84";
-//const API_BASE_URL = "http://veera.905418452515.realhandsonlabs.net";
+// const API_BASE_URL = "http://44.211.156.103:84";
+const API_BASE_URL = "http://themvr.shop";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
