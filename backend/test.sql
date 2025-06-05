26fe74069e61f4ee4efaf3ea45a1feb0b58d7ca0
manudevops726 <amanasa726@gmail.com>
2025-06-06 00:57:53 +0530

    Update test.sql

--- backend/test.sql
+++ backend/test.sql
@@ -28,8 +28,7 @@ CREATE TABLE IF NOT EXISTS `books` (
 -- Dumping data for table `books`
 INSERT INTO `books` (`id`, `title`, `desc`, `price`, `cover`) VALUES
 (1, 'MultiCloud', 'this is mutlicloud with devops cource by veera sir nareshit ', 2343.2, 'https://docs.multy.dev/assets/images/multi-cloud-314609adeec670988dff0882a93fdcb0.png'),
-(2, 'DevOps', 'if you understand the devops it is very easy', 2342.3, 'https://media.licdn.com/dms/image/v2/D5612AQHsA9dJVtZRdw/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1730826289071?e=2147483647&v=beta&t=JGr0gJH6RE9b1Dk7nHAYF14Kmv96Gm519UOy_rSa3xE'),
-(3, 'Wings of Fire', 'Autobiography of Dr. A.P.J. Abdul Kalam, former President and renowned scientist of India.', 199.0, 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/APJ_Abdul_Kalam.jpg/800px-APJ_Abdul_Kalam.jpg');
+(2, 'DevOps', 'if you understand the devops it is very easy', 2342.3, 'https://media.licdn.com/dms/image/v2/D5612AQHsA9dJVtZRdw/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1730826289071?e=2147483647&v=beta&t=JGr0gJH6RE9b1Dk7nHAYF14Kmv96Gm519UOy_rSa3xE');
 
 -- Add primary key for the books table
 ALTER TABLE `books`
@@ -37,11 +36,6 @@ ALTER TABLE `books`
 
 -- Set auto increment for the `id` column
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
--- Create or update the 'admin' user with access from any host
-CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED WITH mysql_native_password BY 'admin123';
-GRANT ALL PRIVILEGES ON test.* TO 'admin'@'%';
-FLUSH PRIVILEGES;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 COMMIT;
