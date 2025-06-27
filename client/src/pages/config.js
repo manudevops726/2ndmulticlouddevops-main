49d71afddc6476fca533933adf212cc283e5c231
manudevops726 <amanasa726@gmail.com>
2025-06-27 08:37:56 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://44.211.156.103:84";
-const API_BASE_URL = "http://52.146.23.94";
+const API_BASE_URL = "http://20.247.179.72";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
